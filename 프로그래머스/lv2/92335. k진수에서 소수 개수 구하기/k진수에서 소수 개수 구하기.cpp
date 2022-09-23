41dee7a84e17fad7ffe9c315d9ad637d0f701a73
DormunDoorlock <43779340+DormunDoorlock@users.noreply.github.com>
2022-09-23 19:09:14 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.02 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cpp
@@ -1,56 +1,40 @@
 #include <string>
+#include <vector>
 #include <cmath>
-
+#include <algorithm>
 using namespace std;
 
-typedef long long ll;
-
-bool prime(ll n) { // 소수 판별
-    if(n < 2) return false;
-        
-    for(int i=2; i<=sqrt(n); ++i) {
-        if(n % i == 0) return false;
+bool isPrime(long long n){
+    printf("%d ",n);
+    if(n<2)
+        return false;  
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0)
+            return false;
     }
-    
     return true;
 }
-
-string convert_num(int n, int k) { // k진수로 변환
-    string ret = "";
-    
-    while(n) {
-        ret += to_string(n % k);
-        n /= k;
-    }
-    
-    return string(ret.rbegin(), ret.rend());
-}
-
-int get_answer(string num) { // 0을 기준으로 parsing하여 소수이면 count
-    string tmp = "";
-    int ret = 0;
-    
-    for(int i=0; i<num.length(); ++i) {
-        if(num[i] == '0' && !tmp.empty()) {
-            ll n = stoll(tmp);
-            if(prime(n)) ret++;
-            tmp = "";
-        }
-        else tmp += num[i];
-    }
-    
-    if(!tmp.empty()) {
-        ll n = stoll(tmp);
-        if(prime(n)) ret++;
-    }
-    
-    return ret;
-}
-
 int solution(int n, int k) {
     int answer = 0;
-    
-    string num = convert_num(n, k);
-    
-    return get_answer(num);
+
+    string s = "";
+    while(n > 0) {
+        s += to_string(n % k);
+        n /= k;
+    }
+    reverse(s.begin(),s.end());
+    string tmp = "";
+    for (char c : s) { 
+        if (c == '0') { 
+            if (!tmp.empty() && isPrime(stoll(tmp))) {
+                answer++; 
+            } 
+            tmp = ""; 
+        } 
+        else tmp += c; 
+    }
+    if (!tmp.empty() && isPrime(stoll(tmp))) {
+        answer++;
+    } 
+    return answer;
 }
